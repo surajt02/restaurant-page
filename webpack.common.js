6fadc9dd2f1a131b180d6c372d800d55d95f3de3
surajt02 <t.surajt02@gmail.com>
2025-05-01 13:02:44 -0400

    webpack merge

--- webpack.common.js
+++ webpack.common.js
@@ -2,22 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      title: 'Production',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  devtool: 'eval-source-map',
-  devServer: {
-    watchFiles: ['./src/template.html'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html',
-    }),
-  ],
   module: {
     rules: [
       {
